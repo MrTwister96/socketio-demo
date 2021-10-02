cea8ddd5ab9760bab0d97ef5fd8f85b7cb48b09c
Schalk <schalkolivier@vegamine.com>
2021-10-02 20:19:01 +0200

    add eventhandler for enter chats button

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 // Imports
-import store from "./store";
+import store from "./store.js";
+import { goToChatPage } from "./ui.js";
 
 // Connect to Socket.io Server
 const socket = io("/");
@@ -15,9 +16,16 @@ socket.on("hello-client", () => {
     socket.emit("hello-server");
 });
 
-// Get name input
+// name input element
 const nameInput = document.querySelector(".introduction_page_name_input");
 
 nameInput.addEventListener("keyup", (event) => {
     store.setUsername(event.target.value);
+});
+
+// enter chats button element
+const chatPageButton = document.getElementById("enter_chats_button");
+
+chatPageButton.addEventListener("click", () => {
+    goToChatPage();
 });
