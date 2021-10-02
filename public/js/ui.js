5f5f6df9548dace5f641689d6d82089b7c35c7b9
Schalk <schalkolivier@vegamine.com>
2021-10-02 21:22:58 +0200

    Logic for new group message

--- public/js/ui.js
+++ public/js/ui.js
@@ -2,21 +2,25 @@ import { getChatbox } from "./elements.js";
 import store from "./store.js";
 
 export const goToChatPage = () => {
-    // Get page elements
-    const introductionPage = document.querySelector(".introduction_page");
-    const chatPage = document.querySelector(".chat_page");
+    if (store.getUsername() !== "") {
+        // Get page elements
+        const introductionPage = document.querySelector(".introduction_page");
+        const chatPage = document.querySelector(".chat_page");
 
-    // Hide Introduction Page
-    introductionPage.classList.add("display_none");
+        // Hide Introduction Page
+        introductionPage.classList.add("display_none");
 
-    // Display Chat page
-    chatPage.classList.remove("display_none");
-    chatPage.classList.add("display_flex");
+        // Display Chat page
+        chatPage.classList.remove("display_none");
+        chatPage.classList.add("display_flex");
 
-    const username = store.getUsername();
-    updateUsername(username);
+        const username = store.getUsername();
+        updateUsername(username);
 
-    createGroupChatbox();
+        createGroupChatbox();
+    } else {
+        alert("Enter valid name!");
+    }
 };
 
 const chatboxId = "group-chat-chatbox";
@@ -35,6 +39,27 @@ const createGroupChatbox = () => {
 
     const chatboxesContainer = document.querySelector(".chatboxes_container");
     chatboxesContainer.appendChild(chatbox);
+
+    const newMessageInput = document.getElementById(chatboxInputId);
+    newMessageInput.addEventListener("keyup", (event) => {
+        const key = event.key;
+
+        if (key === "Enter") {
+            const author = store.getUsername();
+            const messageContent = event.target.value;
+            // Send message to socket.io server
+            if (messageContent !== "" && !messageContent.startsWith(" ")) {
+                newMessageInput.value = "";
+                console.log({
+                    author,
+                    messageContent,
+                });
+            } else {
+                newMessageInput.value = "";
+                alert("Enter valid message!");
+            }
+        }
+    });
 };
 
 const updateUsername = (username) => {
