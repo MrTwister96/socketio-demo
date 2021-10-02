00a2a98cc6f887c193fb9416b50dc4b9a718fe20
Schalk <schalkolivier@vegamine.com>
2021-10-02 18:36:26 +0200

    Initial Socket IO Connection Handling

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
-const express = require("express");
-const http = require("http");
+// Import Express
+import express from "express";
+// Import HTTP
+import { createServer } from "http";
+// Import Socket.io
+import { Server } from "socket.io";
+// Import dotenv
+import dotenv from "dotenv";
 
-const PORT = 5000;
+// Configure dotenv
+dotenv.config();
 
+// Set HTTP port to listen on
+const PORT = process.env.EXPRESS_PORT || 5000;
+
+// Initialize Express
 const app = express();
-const server = http.createServer(app);
+// Create Server With Express App
+const server = createServer(app);
 
+// Define static file location
 app.use(express.static("public"));
 
+// Default Route
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-const io = require("socket.io")(server);
+// Initialize Socket.io server
+const io = new Server(server);
 
+// Handle Incomming Socket.Io Connections
+io.on("connection", (socket) => {
+    //Log successfull connections
+    console.log(`CLIENT CONNECTED: SOCKETID: ${socket.id}`);
+
+    // Socket.io Disconnection
+    socket.on("disconnect", (reason) => {
+        console.log(
+            `CLIENT DISCONNECTED. REASON: ${reason} SOCKETID: ${socket.id}`
+        );
+    });
+});
+
+// Start HTTP Server
 server.listen(PORT, () => {
     console.log(`SERVER STARTED ON ${PORT}`);
 });
