99b5388d590337ec9f67c2e48d02230061fa0b33
Schalk <schalkolivier@vegamine.com>
2021-10-02 21:22:15 +0200

    message container function

--- public/js/elements.js
+++ public/js/elements.js
@@ -12,17 +12,22 @@ export const getChatbox = ({
         <div class="chatbox_label_container">
             <p class="chatbox_label">${chatboxLabel}</p>
         </div>
-        <div class="messages_container" id="${chatboxMessagesId}">
-            <div class="message_container">
-                <p class="message_paragraph">
-                    <span class="message_author">Martin: </span>Hello Guys
-                </p>
-            </div>
-        </div>
+        <div class="messages_container" id="${chatboxMessagesId}"></div>
         <div class="new_message_input_container">
             <input class="new_message_input" id="${chatboxInputId}" placeholder="Enter your message..."/>
         </div>
     `;
 
     return chatboxContainer;
+};
+
+export const getGroupChatMessage = ({ author, messageContent }) => {
+    const messageContainer = document.createElement("div");
+    messageContent.classList.add("message_container");
+
+    messageContainer.innerHTML = `
+        <p class="message_paragraph">
+            <span class="message_author">${author}: </span>${messageContent}
+        </p>
+    `;
 };
