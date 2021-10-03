52f7c4337c11725654a778cc3e556045f8b18bdc
Schalk <schalkolivier@vegamine.com>
2021-10-03 12:58:27 +0200

    Code cleanup

--- public/js/socketHandler.js
+++ public/js/socketHandler.js
@@ -3,6 +3,7 @@ import { appendGroupChatMessage } from "./ui.js";
 
 let socket = null;
 
+////////// EXPORTED FUNCTIONS //////////
 const connectToSocketIoServer = () => {
     socket = io("/");
 
@@ -16,12 +17,13 @@ const connectToSocketIoServer = () => {
     });
 };
 
-const registerActiveSession = () => {
-    socket.emit("register-new-user", { username: store.getUsername() });
-};
-
 const sendGroupChatMessage = (author, messageContent) => {
     socket.emit("group-chat-message", { author, messageContent });
+};
+
+////////// LOCAL FUNCTIONS //////////
+const registerActiveSession = () => {
+    socket.emit("register-new-user", { username: store.getUsername() });
 };
 
 export default { connectToSocketIoServer, sendGroupChatMessage };
--- server.js
+++ server.js
@@ -1,37 +1,19 @@
-// Import Express
 import express from "express";
-// Import HTTP
 import { createServer } from "http";
-// Import Socket.io
 import { Server } from "socket.io";
-// Import dotenv
 import dotenv from "dotenv";
-
-// Configure dotenv
 dotenv.config();
 
-// Set HTTP port to listen on
-const PORT = process.env.EXPRESS_PORT || 5000;
-
-// Initialize Express
 const app = express();
-// Create Server With Express App
 const server = createServer(app);
-
-// Define static file location
 app.use(express.static("public"));
-
-// Default Route
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-// Initialize Socket.io server
+// Start Socket.IO
 const io = new Server(server);
-
 let connectedPeers = [];
-
-// Handle Incomming Socket.Io Connections
 io.on("connection", (socket) => {
     socket.on("register-new-user", ({ username }) => {
         connectedPeers = [...connectedPeers, { username, socketId: socket.id }];
@@ -49,6 +31,7 @@ io.on("connection", (socket) => {
 });
 
 // Start HTTP Server
+const PORT = process.env.EXPRESS_PORT || 5000;
 server.listen(PORT, () => {
     console.log(`SERVER STARTED ON ${PORT}`);
 });
